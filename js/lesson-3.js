50c18f1773e2e8e8240d4d3af66231d60b240b39
Oleksandr <zholudenko23@gmail.com>
2025-03-05 01:34:58 +0300

    lesson-3 task 1-5

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -3,40 +3,50 @@
 // квадрати кожного елементу вхідного масиву.
 // Очікуваний результат: [1, 4, 9, 16, 25].
 
-// const numbers = [1, 2, 3, 4, 5]
+const numbers = [1, 2, 3, 4, 5].map(number => number * number);
+
+/* console.log(numbers); */
 
 //Завдання 2:
 // Дано масив об'єктів. Створіть новий масив, що містить всі значення
 // з масивів values кожного об'єкту, збережених в одному масиві.
 // Очікуваний результат: [1, 2, 3, 4, 5, 6, 7, 8, 9].
 
-// const data = [
-//  { id: 1, values: [1, 2, 3] },
-//  { id: 2, values: [4, 5, 6] },
-//  { id: 3, values: [7, 8, 9] },
-// ];
+const data = [
+  { id: 1, values: [1, 2, 3] },
+  { id: 2, values: [4, 5, 6] },
+  { id: 3, values: [7, 8, 9] },
+].flatMap(item => item.values);
+
+/* console.log(data); */
 
 //Завдання 3:
 // Дано масив об'єктів.
 // Перевірте, чи є хоча б один об'єкт з віком менше 20 років.
 // Очікуваний результат: true.
 
-// const people = [
-//  { name: 'John', age: 27 },
-//  { name: 'Jane', age: 31 },
-//  { name: 'Bob', age: 19 },
-// ];
+const people = [
+  { name: 'John', age: 27 },
+  { name: 'Jane', age: 31 },
+  { name: 'Bob', age: 19 },
+].some(user => user.age < 20);
+
+/* console.log(people); */
 
 //Завдання 4:
 // Дано масив чисел [2, 4, 6, 8, 10].
 // Перевірте, чи є кожен елемент масиву парним. Очікуваний результат: true.
 
-// const numbers = [2, 4, 6, 8, 10];
+const nUmbers = [2, 4, 6, 8, 10].every(item => item % 2 === 0);
+
+/* console.log(nUmbers); */
 
 //Завдання 5:
 // Знайдіть перше непарне число
 
-// const numbers = [2, 1, 6, 8, 9, 10, 12];
+const nuMbers = [2, 1, 6, 8, 9, 10, 12].find(number => number % 2 === 1);
+
+/* console.log(nuMbers); */
 
 //Завдання 6:
 // Відсортуйте масив чисел [4, 2, 5, 1, 3]
